ef56a1fb2711e46e252f41d219a34654e7558e70
thaabit <mohammedthaabitjacobs@gmail.com>
2021-11-03 08:40:55 +0200

    hello

---
+++ tip_form/js/app.js
@@ -0,0 +1,8 @@
+//Things I learned: How to dynamically include options into an HTML form
+//Things I changed: I created two functions to handle input. One for errors. One for calculating the tip.
+//I broke out the feedback into three different feeds. Before, all three would show for just one error.
+//Added a validator to only calculate if feedback wasn't received.
+//Added a toFixed method to the output
+// Made input disappear after submitting
+//Bug. I had setInterval in my code instead of setTimeout. 
+
